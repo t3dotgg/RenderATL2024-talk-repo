45cd3541a618ae5656ce02516f8babf5a31d0b81
Theo Browne <me@t3.gg>
2024-06-09 18:12:52 -0700

    Add counter

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-app';
---
+++ angular-app/src/app/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+}
