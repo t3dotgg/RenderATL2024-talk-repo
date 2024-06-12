39b93b7aceaedab7fc85a3b93026f882bd4f6f69
Theo Browne <me@t3.gg>
2024-06-12 04:43:47 -0700

    working mongo

--- next/src/app/bash/page.tsx
+++ next/src/app/bash/page.tsx
@@ -1,20 +1,21 @@
 import { revalidatePath } from "next/cache";
 import sql from "../db";
 import { seedDB } from "../db/seed";
+import { getMongo } from "../db/mongo";
 
-let content = "null";
+let content: unknown = null;
 
 function ContentComponent() {
-  if (content === "null") return null;
+  if (!content) return null;
 
   return (
     <div>
-      {JSON.parse(content).length} rows
+      {(content as any[])?.length} rows
       {/* Script tag with content */}
       <script
         dangerouslySetInnerHTML={{
           __html: `
-      window.content = ${content};
+      window.content = ${JSON.stringify(content)};
     `,
         }}
       />
@@ -22,23 +23,50 @@ function ContentComponent() {
   );
 }
 
-async function copyPrimary() {
+async function getContentsOfLegacyDB() {
   "use server";
 
-  const rows = await sql`SELECT * FROM user WHERE id > ${5} AND id <= ${10};`;
-  content = JSON.stringify(rows);
+  content = await sql`SELECT * FROM user;`;
+
+  revalidatePath("/bash");
+}
+
+async function moveDataToMongo() {
+  "use server";
+
+  // const rows = await sql`SELECT * FROM user WHERE id > ${5} AND id <= ${10};`;
+  // content = JSON.stringify(rows);
+
+  const mongoClient = await getMongo();
+  const results = await mongoClient
+    .collection("users")
+    .insertMany(content as any[]);
+
+  console.log(results);
 
   revalidatePath("/bash");
 }
 
 const btnClass = "text-xl border bg-white text-black p-2 rounded-md";
 
-export default function BashPage() {
+export default async function BashPage() {
+  const mongoClient = await getMongo();
+  const results = await mongoClient.collection("users").find({}).toArray();
+
+  console.log(results);
+
   return (
     <form className="flex flex-col gap-2 p-4">
       <ContentComponent />
-      <button type="submit" formAction={copyPrimary} className={btnClass}>
+      <button
+        type="submit"
+        formAction={getContentsOfLegacyDB}
+        className={btnClass}
+      >
         Run server code
+      </button>
+      <button type="submit" formAction={moveDataToMongo} className={btnClass}>
+        Run Migration
       </button>
       <button
         type="submit"
