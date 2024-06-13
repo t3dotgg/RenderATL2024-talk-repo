ae87feb1594f5fa196d723f82b01ae5a1a2704cc
Theo Browne <me@t3.gg>
2024-06-13 02:35:00 -0400

    more cleanup

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col">
-        <a href="/api/app" className="font-bold text-xl">
-          Link to a totally normal Next app
-        </a>
-      </div>
+      <div className="flex flex-col">A totally normal Next app</div>
     </main>
   );
 }
